961e9a18f63c97c1d1ef65ea143d7921fa428051
Fabrice Douchant <vamp.higher@gmail.com>
2012-11-16 18:00:57 +0100

    enhance documentation generation

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from distutils.core import setup
 
+spip_url = "http://fabricedouchant.com/spip.php?article52&lang=en"
+try:
+    from html2rest import html2rest
+    from StringIO import StringIO
+    
+    # read content of article (plain html template) and convert to restructuredText for long_description
+    stringIO = StringIO()
+    html2rest(open(spip_url+'&page=article_plain_html').read(), writer=stringIO, preprocess=lambda html, encoding: html.replace('<code', '<pre').replace('</code>', '</pre>'))
+    long_desc = stringIO.getvalue()
+except ImportError:
+    print "Couldn't load html2rest, use README.txt as description"
+    long_desc = open('README.txt').read()
+
 import myPyApps
 
 setup(
@@ -8,11 +21,11 @@ setup(
     author='Fabrice Douchant',
     author_email='vamp.higher@gmail.com',   
     description='Allow quick Python programs development',
-    long_description=open('README.txt').read(),
+    long_description=long_desc,
     license='GNU GPLv3',
     platforms = ["any"],
     keywords='framework tools',
-    url='http://fabricedouchant.com/spip.php?article52&lang=en',
+    url=spip_url,
     packages=['myPyApps'],
     package_data={
        'myPyApps': ['config/*.default'],
